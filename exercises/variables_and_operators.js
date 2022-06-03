1eabf4e08a5abb75dd01dceb72e6c18be88a20c9
tuta1612 <lucassaavedra@yahoo.com.ar>
2022-06-02 22:49:01 -0300

    parte 1 variables y operadores

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,11 @@
+var num1 = 5
+var num2 = 7;
+var num3 = num1+num2;
+
+var text1 = "Hola ";
+var text2 = "mundo"
+var text3 = text1+text2;
+
+var text4 = "Hola mundo";
+var text5 = "Buenas gracias, muchas noches"
+var contadorTexto = (text4+text5).length;
